a965083fd382540d77c0c68fe2a243228947cff3
lokesh.pahal <lokesh.pahal@prodigi.io>
2015-05-19 09:39:01 +0530

    Adding IE Only Support

--- gplaceholder.js
+++ gplaceholder.js
@@ -1,6 +1,6 @@
-(function ($,d) {
+(function ($, d) {
 	$.fn.extend({
-		resetGP: function(){
+		resetGP:function() {
 			var oldOpts = $(this).data('Opts');
 			$this = $(this);
 			$this.next().remove();
@@ -10,13 +10,21 @@
 	
 	$.fn.gplaceholder = function( options ) {
 	var settings = $.extend({
-		color: "#000"
+		color: "#000",
+        onlyIE: true
 	}, options );
+    var e = window.navigator.userAgent;
+    var r = e.indexOf("MSIE ");
+    if (r > 0 || navigator.userAgent.match(/Trident.*rv\:11\./)) {
+        var s = parseInt(e.substring(r + 5, e.indexOf(".", r)));
+        if (s > 9) return !0
+    } else if(1 == settings.onlyIE) return !0;
+        
 	$(this).data('Opts',options);
 	this.each(function(){
-		p = $(this).attr('placeholder');
+		var p = $(this).attr('placeholder');
 		if($.trim(p)!=''){
-			$(this).css({color:settings.color,background:settings.background})
+			$(this).css({color: settings.color,background: settings.background})
 			$(this).val(p)
 			$(this).bind('focus',function(){
 				v = $(this).val();
